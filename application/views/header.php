7592037247fbd4b65858b126a6dd02b98336130c
selviani150399 <52683627+selviani150399@users.noreply.github.com>
2020-01-06 13:24:53 +0800

    header

--- application/views/header.php
+++ application/views/header.php
@@ -144,7 +144,7 @@
       <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTA" aria-expanded="true" aria-controls="collapseTwo">
           <i class="fas fa-fw fa-cog"></i>
-          <span>Tugas AKhir</span>
+          <span>Tugas Akhir</span>
         </a>
         <div id="collapseTA" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
