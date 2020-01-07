ddc014ebaea768a12351da29e8d5809496637441
ramawahyu112 <52642373+ramawahyu112@users.noreply.github.com>
2020-01-07 12:01:32 +0800

    proposalta

--- application/views/proposalta.php
+++ application/views/proposalta.php
@@ -123,7 +123,7 @@
                     <td>
                       <a href="#" class="btn btn-sm btn-info" data-toggle="modal" data-target="#modal_edit<?php echo $row->NoProposal;?>"> Update </a>
                  
-                      <a href="<?php echo site_url('tugasakhir/deletekaprodi/'.$row->NoProposal);?>" class="btn btn-sm btn-danger">Delete</a>
+                      <a href="<?php echo site_url('tugasakhir/deleteproposalta/'.$row->NoProposal);?>" class="btn btn-sm btn-danger">Delete</a>
                         
                       </td>
                   </tr>
@@ -148,9 +148,9 @@
             $NoProposal=$i['NoProposal'];
             $JudulProposal=$i['JudulProposal'];
             $TahunProposal=$i['TahunProposal'];
-            $NamaMahasiswa=$i['NamaMahasiswa'];
-            $Pembimbing1=$i['Pembimbing1'];
-            $Pembimbing2=$i['Pembimbing2'];
+            $NIM=$i['NIM'];
+            $NIPPembimbing1=$i['NIPPembimbing1'];
+            $NIPPembimbing2=$i['NIPPembimbing2'];
         ?>
         <div class="modal fade" id="modal_edit<?php echo $NoProposal;?>" tabindex="-1" role="dialog" aria-labelledby="largeModal"
          aria-hidden="true">
@@ -158,48 +158,61 @@
             <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
-                <h4 class="modal-title" id="myModalLabel">Edit Program Studi</h4>
+                <h4 class="modal-title" id="myModalLabel">Edit Proposal TA</h4>
             </div>
-            <form class="form-horizontal" method="post" action="<?php echo site_url('tugasakhir/addkaprodi/'.$KodeProdi.'/'.$row->NIP);?>">
+            <form class="form-horizontal" method="post" action="<?php echo site_url('tugasakhir/addproposalta/'.$NoProposal);?>">
                 <div class="modal-body">
- 
-
-                   <div class="form-group">
-                        <label class="control-label col-xs-3" >Program Studi</label>
+    
+                    <div class="form-group">
+                        <label class="control-label col-xs-3" >Judul Proposal</label>
                         <div class="col-xs-8">
-                            <select name="KodeProdi" class="form-control">
-                            <option value="0">-Pilih Program Studi-</option>
-                            <?php foreach($prodi->result() as $rowprodi):?>
-                                <option value="<?php $prodis=$rowprodi->KodeProdi; echo $prodis; ?>"
-                                   <?php if($KodeProdi==$prodis){ echo 'selected';} ?>
-                                  ><?php echo $rowprodi->NamaProdi;?></option>
+                            <input name="JudulProposal" class="form-control"  value="<?php echo $JudulProposal;?>" type="text" placeholder="Masukkan Judul Proposal" required>
+                        </div>
+                    </div>
+                      <div class="form-group">
+                        <label class="control-label col-xs-3" >Tahun Proposal</label>
+                        <div class="col-xs-8">
+                            <input name="TahunProposal" class="form-control" type="text" value="<?php echo $TahunProposal;?>" placeholder="Masukkan Tahun Proposal" required>
+                        </div>
+                    </div>
+
+                  <div class="form-group">
+                        <label class="control-label col-xs-3" >Nama Mahasiswa</label>
+                        <div class="col-xs-8">
+                            <select name="NIM" class="form-control">
+                            <option value="0">-Pilih Mahasiswa-</option>
+                            <?php foreach($mahasiswa->result() as $rowmahasiswa):?>
+                                <option value="<?php $nims=$rowmahasiswa->NIM; echo $nims; ?>"  <?php if($NIM==$nims){ echo 'selected';} ?>
+                                ><?php echo $rowmahasiswa->NamaMahasiswa;?></option>
                             <?php endforeach;?>
                         </select>
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <label class="control-label col-xs-3" >Ketua Program Studi</label>
+                        <label class="control-label col-xs-3" >Pembimbing 1</label>
                         <div class="col-xs-8">
-                            <select name="NIP" class="form-control">
-                            <option value="0">-Pilih Ketua Program Studi-</option>
+                            <select name="NIPPembimbing1" class="form-control">
+                            <option value="0">-Pilih Dosen-</option>
                             <?php foreach($dosen->result() as $rowdosen):?>
-                                <option value="<?php $dosens=$rowdosen->NIP; echo $dosens; ?>"
-                                  <?php if($NIP==$dosens){ echo 'selected';} ?>
-                                  ><?php echo $rowdosen->NamaDosen;?></option>
+                                <option value="<?php $nips=$rowdosen->NIP; echo $nips; ?>"  <?php if($NIPPembimbing1==$nips){ echo 'selected';} ?>><?php echo $rowdosen->NamaDosen;?></option>
                             <?php endforeach;?>
                         </select>
                         </div>
                     </div>
 
-                    <div class="form-group">
-                        <label class="control-label col-xs-3" >Periode</label>
+                     <div class="form-group">
+                        <label class="control-label col-xs-3" >Pembimbing 2</label>
                         <div class="col-xs-8">
-                            <input name="Periode" class="form-control" value="<?php echo $Periode;?>" type="text" placeholder="Masukkan Periode Jabatan" required>
+                            <select name="NIPPembimbing2" class="form-control">
+                            <option value="0">-Pilih Dosen-</option>
+                            <?php foreach($dosen->result() as $rowdosen):?>
+                                <option value="<?php $nips2=$rowdosen->NIP; echo $nips2;?>" <?php if($NIPPembimbing2==$nips2){ echo 'selected';} ?>><?php echo $rowdosen->NamaDosen;?></option>
+                            <?php endforeach;?>
+                        </select>
                         </div>
                     </div>
 
- 
  
                 </div>
  
